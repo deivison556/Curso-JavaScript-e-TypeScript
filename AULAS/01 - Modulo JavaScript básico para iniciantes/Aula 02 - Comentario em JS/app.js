8be4bf39f27abab5ea424f9d958771630ff8d638
deivison556 <deivison10gracindo@outlook.com>
2025-03-25 16:55:57 -0300

    Aula 02 - Comentario em JS - concluído

---
+++ AULAS/01 - Modulo JavaScript básico para iniciantes/Aula 02 - Comentario em JS/app.js
@@ -0,0 +1,10 @@
+// Isso é um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
+/*
+  Esse é um comentário em bloco
+*/
