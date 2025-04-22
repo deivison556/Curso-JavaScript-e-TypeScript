812a531b78b5ed6f381ccddc9cf4d50ead4d0b85
deivison556 <deivison10gracindo@outlook.com>
2025-04-22 14:10:45 -0300

    Aula 07 - Tipos de dados primitivos em JavaScript

---
+++ AULAS/01 - Modulo JavaScript básico para iniciantes/Aula 07 - Tipos de dados primitivos em JavaScript/app.js
@@ -0,0 +1,24 @@
+// Tipos de dados primitivos
+// Tipo string
+const nome = 'Deivison';
+const nome1 = "Deivison";
+const nome2 = `Deivison`;
+
+// Tipo number
+const num1 = 10;
+const num2 = 10.52;
+
+// Tipo undefined;
+let nomeAluno; // não aponta para local nenhum na memória
+
+
+// Tipo null
+let sobrenomeAluno = null; // Nulo - > não aponta para local nenhum na memória
+
+// Tipo boolean
+const aprovado = true;
+const reprovado = false; 
+
+// Checando o tipo do dado
+console.log(typeof nome, nome); // checando o tipo e o valor 
+console.log(typeof sobrenomeAluno, sobrenomeAluno); // checando o tipo e o valor 
