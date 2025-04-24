827e06b7e7b9e129604fdc23e256ce97279bdb02
deivison556 <deivison10gracindo@outlook.com>
2025-04-24 09:26:03 -0300

    Aula 09 - Alert, confirm e prompt no navegador

---
+++ AULAS/01 - Modulo JavaScript básico para iniciantes/Aula 09 - Alert, confirm e prompt no navegador/js/app.js
@@ -0,0 +1,8 @@
+/* alert, confirm e prompt */
+alert('Seja bem vindo ao meu site');
+confirm ('Deseja entrar no site?');
+prompt('Digite o seu nome:');
+
+let confirma = confirm('Deseja apagar?');
+let num1 = Number(prompt('Digite um número:'));
+let num2 = Number(prompt('Digite um segundo número:'));
